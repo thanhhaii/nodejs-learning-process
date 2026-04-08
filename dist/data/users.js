13e39e7a038289c0021f1b244ea7875d785aca19
Hai Lam <hailamnguyenthanh@gmail.com>
2026-04-08 17:31:34 +0700

    remove folder dist

--- dist/data/users.js
+++
@@ -1,5 +0,0 @@
-export const users = [
-    { id: 1, name: "Alice", role: "Frontend Dev" },
-    { id: 2, name: "Bob", role: "Node.js Newbie" },
-];
-export const findUserById = (id) => users.find((u) => u.id === id);
--- dist/server.js
+++
@@ -1,5 +0,0 @@
-import { app } from "./app.js";
-import { PORT } from "./config/constants.js";
-app.listen(PORT, () => {
-    console.log(`🚀 Server đang chạy tại: http://localhost:${PORT}`);
-});
